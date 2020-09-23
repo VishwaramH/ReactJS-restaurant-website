7ec77db0b98d626590e243c850020ee2ebe6825f
Vishwaram H <kingvichu362@gmail.com>
2020-09-23 14:42:23 +0530

    Fetch from REST API server exercise

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,6 +4,7 @@ import { Card, CardImg, CardBody, CardTitle, CardText, Breadcrumb, BreadcrumbIte
 import { Link } from 'react-router-dom';
 import { LocalForm, Control, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 const required = (val) => val && (val.length);
 const minLength = (len) => (val) => val && (val.length>=len);
@@ -91,7 +92,7 @@ class CommentForm extends Component {
     if(dish!=null) {
       return(
         <Card>
-          <CardImg width="100%" src={dish.image} alt={dish.name} />
+          <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
           <CardBody>
             <CardTitle>{dish.name}</CardTitle>
             <CardText>{dish.description}</CardText>
